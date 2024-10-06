a5bc8960d63464d4bc605c3ddeed54f4347db3a4
ajmalali10 <officialajmalali10@gmail.com>
2024-10-06 22:46:13 +0530

    added new strapi version

--- src/lib/stripe.js
+++ src/lib/stripe.js
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRAPE_SECRET_KEY, {
-  apiVersion: "2024-09-30.acacia",
+export const stripe = new Stripe(process.env.STRAPE_SECRET_KEY ?? "", {
+  apiVersion: "2024-04-10",
   typescript: false,
 });
