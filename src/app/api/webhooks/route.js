af000fdd4c0296d5dc3b99839f87da8737b6cc0c
ajmalali10 <officialajmalali10@gmail.com>
2024-10-07 16:00:05 +0530

    added webhook secret

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,82 +1,57 @@
-// import OrderReceivedEmail from "@/components/emails/OrderReceivedEmail";
-import { updateOrder } from "@/db/orderDB";
-import { stripe } from "@/lib/stripe";
-import { console } from "inspector";
-import { headers } from "next/headers";
 import { NextResponse } from "next/server";
-// import { Resend } from "resend";
+import Stripe from "stripe";
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
+  apiVersion: "2024-09-30.acacia",
+  typescript: false,
+});
 
+const buffer = (req) => {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+
+    req.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    req.on("end", () => {
+      resolve(Buffer.concat(chunks));
+    });
+
+    req.on("error", reject);
+  });
+};
 export async function POST(req) {
-  console.log(req);
+  const sig = headers().get("Stripe-Signature");
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  let event;
   try {
-    const body = await req.text();
-    const signature = headers().get("stripe-signature");
+    if (!sig || !webhookSecret) return;
+    const body = await buffer(req);
 
-    if (!signature) {
-      return new Response("Invalid signature", { status: 400 });
-    }
-
-    const event = stripe.webhooks.constructEvent(
+    event = stripe.webhooks.constructEvent(
       body,
-      f057dad265fe260c8d0f42884a046e9f3cc599772ce701d83f095a9045f17,
+      sig,
       process.env.STRIPE_WEBHOOK_SECRET
     );
-
-    if (event.type === "checkout.session.completed") {
-      if (!event.data.object.customer_details?.email) {
-        throw new Error("Missing user email");
-      }
-
-      const session = event.data.object;
-
-      const { userId, orderId } = session.metadata || {
-        userId: null,
-        orderId: null,
-      };
-
-      if (!userId || !orderId) {
-        throw new Error("Invalid request metadata");
-      }
-
-      const billingAddress = session.customer_details.address;
-      const shippingAddress = session.shipping_details.address;
-
-      const updatedOrder = await updateOrder(
-        orderId,
-        session,
-        shippingAddress,
-        billingAddress
-      );
-
-      //   await resend.emails.send({
-      //     from: "CaseCobra <hello@joshtriedcoding.com>",
-      //     to: [event.data.object.customer_details.email],
-      //     subject: "Thanks for your order!",
-      //     react: OrderReceivedEmail({
-      //       orderId,
-      //       orderDate: updatedOrder.createdAt.toLocaleDateString(),
-      //       // @ts-ignore
-      //       shippingAddress: {
-      //         name: session.customer_details.name,
-      //         city: shippingAddress.city,
-      //         country: shippingAddress.country,
-      //         postalCode: shippingAddress.postal_code,
-      //         street: shippingAddress.line1,
-      //         state: shippingAddress.state,
-      //       },
-      //     }),
-      //   });
-    }
-
-    return NextResponse.json({ result: event, ok: true });
   } catch (err) {
-    console.error(err);
-
-    return NextResponse.json(
-      { message: "Something went wrong", ok: false },
-      { status: 500 }
-    );
+    console.log(`❌ Error message: ${err.message}`);
+    return new Response(`Webhook Error: ${err.message}`, { status: 400 });
   }
+
+  // Handle the checkout.session.completed event
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object;
+
+    // Fulfill the purchase...
+    console.log("SESSION: ", session);
+  }
+
+  return NextResponse.json({ received: true });
 }
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
